2ef99b79c7a9dfef718e1c358ae2e33351a8418c
Maria Zhukova <zhukovamaria2000@gmail.com>
2026-05-24 00:43:55 +0300

    Реализовано форматирование данных при выводе в консоль

---
+++ main-project/TablePrinter.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "Session.h"  
+
+using namespace std;
+
+void printSessions(const vector<Session>& sessions, const string& title);
--- main-project/main.cpp
+++ main-project/main.cpp
@@ -11,27 +11,9 @@
 #include "Session.h"
 #include "FileReader.h"
 #include "constants.h"
+#include "TablePrinter.h"
 
 using namespace std;
-
-// Вывод таблицы
-void printSessions(const vector<Session>& sessions, const string& title) {
-    cout << "\n========================================\n";
-    cout << title << "\n";
-    cout << "========================================\n";
-    cout << left << setw(12) << "НАЧАЛО" << setw(12) << "КОНЕЦ"
-        << setw(15) << "ПОЛУЧЕНО" << setw(15) << "ОТПРАВЛЕНО"
-        << "ПРОГРАММА\n";
-    cout << string(80, '-') << "\n";
-
-    for (const auto& s : sessions) {
-        cout << left << setw(12) << s.startTime() << setw(12) << s.endTime()
-            << setw(15) << s.received << setw(15) << s.sent
-            << s.program << "\n";
-    }
-
-    if (sessions.empty()) cout << "Нет данных\n";
-}
 
 int main() {
     setlocale(LC_ALL, "Russian");
