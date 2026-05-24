ca463cca323e178ca4aa046925960329d3e208e5
Maria Zhukova <zhukovamaria2000@gmail.com>
2026-05-24 13:55:51 +0300

    изменения в ветке main в выводе группы

--- main-project/main.cpp
+++ main-project/main.cpp
@@ -8,12 +8,82 @@
 #include <algorithm>
 #include <cctype>
 
-#include "Session.h"
-#include "FileReader.h"
-#include "constants.h"
-#include "TablePrinter.h"
-
 using namespace std;
+
+struct Session {
+    int startSec, endSec;  // Время в секундах для простоты сравнения
+    long long received, sent;
+    string program;
+
+    bool contains(const string& name) const {
+        string p = program, n = name;
+        transform(p.begin(), p.end(), p.begin(), ::tolower);
+        transform(n.begin(), n.end(), n.begin(), ::tolower);
+        return p.find(n) != string::npos;
+    }
+
+    string startTime() const {
+        int h = startSec / 3600;
+        int m = (startSec % 3600) / 60;
+        int s = startSec % 60;
+        char buf[9];
+        sprintf(buf, "%02d:%02d:%02d", h, m, s);
+        return buf;
+    }
+
+    string endTime() const {
+        int h = endSec / 3600;
+        int m = (endSec % 3600) / 60;
+        int s = endSec % 60;
+        char buf[9];
+        sprintf(buf, "%02d:%02d:%02d", h, m, s);
+        return buf;
+    }
+};
+
+// Преобразует "чч:мм:сс" в секунды
+int toSeconds(const string& time) {
+    int h, m, s;
+    char c1, c2;
+    istringstream(time) >> h >> c1 >> m >> c2 >> s;
+    return h * 3600 + m * 60 + s;
+}
+
+// Разбор строки файла
+bool parseLine(const string& line, Session& s) {
+    istringstream iss(line);
+    string startStr, endStr;
+
+    if (!(iss >> startStr >> endStr >> s.received >> s.sent)) return false;
+
+    s.startSec = toSeconds(startStr);
+    s.endSec = toSeconds(endStr);
+
+    getline(iss, s.program);
+    size_t pos = s.program.find_first_not_of(" \t");
+    if (pos != string::npos) s.program = s.program.substr(pos);
+
+    return true;
+}
+
+// Вывод таблицы
+void printSessions(const vector<Session>& sessions, const string& title) {
+    cout << "\n========================================\n";
+    cout << title << "\n";
+    cout << "========================================\n";
+    cout << left << setw(12) << "НАЧАЛО" << setw(12) << "КОНЕЦ"
+        << setw(15) << "ПОЛУЧЕНО" << setw(15) << "ОТПРАВЛЕНО"
+        << "ПРОГРАММА\n";
+    cout << string(80, '-') << "\n";
+
+    for (const auto& s : sessions) {
+        cout << left << setw(12) << s.startTime() << setw(12) << s.endTime()
+            << setw(15) << s.received << setw(15) << s.sent
+            << s.program << "\n";
+    }
+
+    if (sessions.empty()) cout << "Нет данных\n";
+}
 
 int main() {
     setlocale(LC_ALL, "Russian");
@@ -21,9 +91,30 @@ int main() {
     cout << "Лабораторная работа №8\n";
     cout << "Вариант: Протокол работы в Интернет\n";
     cout << "Студент: Мария Жукова\n";
-    cout << "Номер группы: 25ИСиТ1д\n\n";
+    cout << "Номер группы: 25ИСиТ1д (очная форма) - изменено в ветке main\n";
+    cout << "Факультет:ФМиИТ\n\n";
 
-    vector<Session> allSessions = readSessionsFromFile(DATA_FILE);
+    ifstream file("protocol.txt");
+    if (!file.is_open()) {
+        cerr << "Ошибка: не удалось открыть protocol.txt\n";
+        return 1;
+    }
+
+    vector<Session> allSessions;
+    string line;
+    int lineNum = 0;
+
+    while (getline(file, line)) {
+        lineNum++;
+        Session s;
+        if (parseLine(line, s)) {
+            allSessions.push_back(s);
+        }
+        else {
+            cerr << "Ошибка в строке " << lineNum << ": " << line << "\n";
+        }
+    }
+    file.close();
 
     if (allSessions.empty()) {
         cout << "Нет данных\n";
